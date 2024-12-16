9d1c2d0236b1643bffe16312c4daf0bc9c5792e2
Dima <dimagr3214@gmail.com>
2024-12-17 01:28:01 +0300

    add result folder and cleaning up the links.txt

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import os
 
-links = []
 
-try:
-    with open("links.txt", "r") as file:
-        for line in file.readlines():
-            links.append(line.strip())
+if __name__ == "__main__":
 
-    for video in links:
-        os.system('.\\yt-dlp.exe -P ".\\result" ' + video)
+    links = []
 
-except Exception:
-    print("Some error occurred")
+    try:
+        with open("links.txt", "r") as file:
+            for line in file.readlines():
+                links.append(line.strip())
 
-print("Process finished")
+        for video in links:
+            os.system('.\\yt-dlp.exe -P ".\\result" ' + video)
+
+        with open("links.txt", "w"):
+            pass
+
+    except Exception:
+        print("Some error occurred")
+
+        with open("error_copy_of_links.txt", "w") as file:
+            for line in links:
+                file.write(line + "\n")
+
+    print("Process finished")
